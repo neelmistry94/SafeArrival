6809ee9eb4e40bad9647f567116ec1b05cc72d64
Neel Mistry <nmistry2@umd.edu>
2015-11-27 01:12:41 -0500

    Initial Commit

---
+++ SafeArrival/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SafeArrival
+//
+//  Created by Neel Mistry on 11/27/15.
+//  Copyright (c) 2015 Neel Mistry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
